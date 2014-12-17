6bf57eabfdf5e47a7d48e66ce4007228655164c8
Grant Kinney <grant@exygy.com>
2014-12-16 21:49:55 -0500

    Updated vvv-site-wizard to 1.2.1

--- vvv-site-wizard.rb
+++ vvv-site-wizard.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class VvvSiteWizard < Formula
   homepage "https://github.com/creativecoder/vvv-site-wizard"
-  url "https://github.com/creativecoder/vvv-site-wizard/archive/1.1.4.tar.gz"
-  sha1 "4f1b13925fec2562095ddb9234dd05610b58aeec"
+  url "https://github.com/creativecoder/vvv-site-wizard/archive/1.2.1.tar.gz"
+  sha1 "e37b5f42f894e4164800e17d019d480ae02ecab8"
 
   def install
     bin.install "vvv"
