51b38435aad6345f345aac706e3b1898f1b0c17a
Grant Kinney <grant@exygy.com>
2015-01-05 11:07:23 -0500

    Updated vv to v20150105

--- vv.rb
+++ vv.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vv < Formula
   homepage "https://github.com/creativecoder/vv/tree/exygy"
-  url "https://github.com/creativecoder/vv/archive/v20150104.tar.gz"
-  sha1 "4515bc7bbdde9dac747af8d25071b67ac95af8f6"
+  url "https://github.com/creativecoder/vv/archive/v20150105.tar.gz"
+  sha1 "edd09be1d0a2bfa48620dfe67da204d3394b62a4"
 
   def install
     bin.install "vv"
